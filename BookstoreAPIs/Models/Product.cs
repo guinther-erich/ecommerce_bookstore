dc15ec1df923d0761fb2fbfb5bc666bf01ed7a37
guinther-erich <67793128+guinther-erich@users.noreply.github.com>
2021-11-17 23:24:17 -0300

    Adicionar arquivos de projeto.

---
+++ BookstoreAPIs/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace BookstoreAPIs.Models
+{
+    public class Book
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public string Img { get; set; }
+    }
+}
