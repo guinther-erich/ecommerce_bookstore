0e97ea0e7c15608821897fe7e21e8925be62dd55
guinther-erich <67793128+guinther-erich@users.noreply.github.com>
2021-11-17 23:46:07 -0300

    Create ToDoContext

---
+++ BookstoreAPIs/Models/ToDoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreAPIs.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> option)
+            : base(option)
+        {
+        }
+
+        public DbSet<Book> todoProducts { get; set;}
+    }
+}
